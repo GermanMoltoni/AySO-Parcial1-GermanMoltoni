0d8a84b81ae785ca386b481be26b48e2ad205a06
German <german.moltoni@gmail.com>
2024-10-17 22:51:13 +0000

    fix: creo el archivo en el primer echo

--- puntof.sh
+++ puntof.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Mi IP Publica es: $(curl -s ifconfig.me)" >> Filtro_Avanzado.txt
+echo "Mi IP Publica es: $(curl -s ifconfig.me)" > Filtro_Avanzado.txt
 echo "Mi usuario es: $(whoami)" >> Filtro_Avanzado.txt
 sudo cat /etc/shadow | grep vagrant | awk -F : '{print "El Hash de mi Usuario es: " $2}' >> Filtro_Avanzado.txt
